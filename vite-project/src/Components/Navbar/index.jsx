b5cd1f51b1f7ef8aff35e7b36fd2d2c2399cb2f8
Mateo Pereira <pereiramateo19@gmail.com>
2023-07-28 13:24:18 -0500

    correccion de la navbar

--- vite-project/src/Components/Navbar/index.jsx
+++ vite-project/src/Components/Navbar/index.jsx
@@ -3,27 +3,35 @@ import NavItem from "../NavItem"
 
 function Navbar() {
     const Navitem = [
-        { to: "/", Navname: "Shope", className: "font-semibold text-lg" },
+        // { to: "/", Navname: "Shope", className: "font-semibold text-lg" },
         { to: "/", Navname: "All" },
         { to: "/clothes", Navname: "Clothes" },
         { to: "/electronics", Navname: "Electronics" },
         { to: "/furnitures", Navname: "Furnitures" },
         { to: "/toys", Navname: "Toys" },
-        { to: "/", Navname: "Others" },
+        { to: "/others", Navname: "Others" },
     ]
     const NavActive = "underline underline-offset-4"
     const NavHover = "hover:underline underline-offset-4"
     return (
         <nav className="flex justify-between items-center z-10 py-4 px-8 font-normal text-sm">
             <ul className="flex items-center gap-3">
-                {Navitem.map(({to, Navname, className})=>(
-                    <NavItem 
-                    key={Navname}
-                    Navname={Navname}
-                    to={to}
-                    NavActive={NavActive}
-                    NavHover={NavHover}
-                    className={className}
+                <li>
+                    <NavLink
+                        to="/"
+                        className="font-semibold text-lg"
+                    >
+                        Shop
+                    </NavLink>
+                </li>
+                {Navitem.map(({ to, Navname, className }) => (
+                    <NavItem
+                        key={Navname}
+                        Navname={Navname}
+                        to={to}
+                        NavActive={NavActive}
+                        NavHover={NavHover}
+                        className={className}
                     />
                 ))}
             </ul>
